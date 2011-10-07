1edfcefc01dfd5878386bf12aa164a923aa5e483
Xavier Deschuyteneer <xavier.deschuyteneer@gmail.com>
2011-10-07 19:35:32 +0200

    modification of the sample.sh to dl all the period and all the groups

--- sample.sh
+++ sample.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-umonstimesheetgrabber -v --html=dl 59 #bac 3 info 
+umonstimesheetgrabber -v -p all -g all --html=dl 59 #bac 3 info toute periode et tout groupe 
 parser -f dl/59--1.html -o dl/59--1.ics 
